aa591c3f8aa894afe4bce451656b13cf9d22fd84
Mehbod <mehrbod.manavi@uni-weimar.de>
2016-11-18 17:34:49 +0100

    Fibonancci  solved

--- Recursion/src/Fibonacci.java
+++ Recursion/src/Fibonacci.java
@@ -1,24 +1,11 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-	//todo: implement a recursive functions whicht gives you the factorial value of a given number
-	/*
-	 * 
-	 * public int testMethod(int x){
-	 * 		if(x >= 5){
-	 * 			return x:
-	 * 		}
-	 * 		else{
-	 * 			return testMethod(x+1);
-	 * 		}
-	 * }
-	 * */
 	public static int fibonacci(int f1, int f2,int f3,int input) {
 		if(input == 0){
 			return f3;
 			}
 			else{
-				
 				f3=f2+f1;
 				System.out.print(f3+",");
 				f1=f2;
@@ -31,7 +18,6 @@ public class Fibonacci {
 		System.out.println("Please Enter the number: ");
 		Scanner scan = new Scanner(System.in);
 		int inputt=scan.nextInt();
-		//System.out.println(fibonacci(f1,f2,f3,inputt));
 		fibonacci(f2,f1,f3,inputt);
 	}
 }
