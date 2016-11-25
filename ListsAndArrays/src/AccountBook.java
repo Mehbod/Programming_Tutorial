af2801c39b44bdbe2bcedbc40180211c99a1ad1a
Mehbod <mehrbod.manavi@uni-weimar.de>
2016-11-25 16:10:30 +0100

    Acoount Bank Done

--- ListsAndArrays/src/AccountBook.java
+++ ListsAndArrays/src/AccountBook.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class AccountBook {
 //intermediate (multidimensional arrays)
@@ -7,5 +8,60 @@ public class AccountBook {
 	//the interest of the next 10 years is stored.
 	//For that save the year and the amount of money. Write a method which takes a certain year as input (e.g. 2019) and an amount of money you would like to store
 	//and returns the saving you would have for this year.
+	
+	/*int[][] arr = new int[5][2];
+	 * pre: year and money
+	 * 2016 -> 100 Euro
+	 * 2017 -> 102 Euro
+	 * ...
+	 * 2026
+	 * 
+	 * 1. Method -> gets year and money and computes and stores array
+	 * 2. Method -> gets year and return money
+	 * */
+	public static double[][] Num=new double[10][2];
+	public static int ComputeArray(int year,double money,int count) {
+
+		
+		if(count == 10){
+			return count;
+			}
+			else{
+
+				Num[count][0]=year;
+				money=money*1.02;
+				Num[count][1]=money;
+				return ComputeArray(++year,money,++count);			 	
+			}
+	}
+
+	public static int CheckArray(int count,int check) {
+		
+		if(count == 10){
+			return count;
+			}
+			else{
+				if(check==Num[count][0]){
+					System.out.println(Num[count][1]);
+				}
+				return CheckArray(++count,check);
+			}
+		
+	}
+	public static void main(String[] args) {
+		System.out.print("Please Enter the Year: ");
+		Scanner scan = new Scanner(System.in);
+		int year=scan.nextInt();
+		System.out.print("Please Enter amount of money: ");
+		double money=scan.nextInt();
+		int x = 0;
+		ComputeArray(year,money,x);
+		System.out.print("Please Enter a year: ");
+		int check=scan.nextInt();
+		int y=0;
+		CheckArray(y,check);
+		
+		
+	}	
 
 }
