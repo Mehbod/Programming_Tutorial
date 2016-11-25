b8a56127d31a531e34e6cb7ca776f8810ed62112
Mehbod <mehrbod.manavi@uni-weimar.de>
2016-11-25 12:45:22 +0100

    Basics array List Done

--- ListsAndArrays/src/BasicsArrayList.java
+++ ListsAndArrays/src/BasicsArrayList.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class BasicsArrayList {
 //beginner
@@ -6,4 +7,37 @@ public class BasicsArrayList {
 	//-write a method where you can add a certain value to an ArrayList
 	//-write a method which prints all the values in an ArrayList
 
+	public static int[]Num=new int[10];
+	public static int AddtoArray(int count) {
+		int temp=count+1;
+		
+		if(count == 10){
+			return count;
+			}
+			else{
+				System.out.print("Please Enter the number "+ temp+": ");
+				Scanner scan = new Scanner(System.in);
+				int inputt=scan.nextInt();
+				Num[count]=inputt;
+				return AddtoArray(++count);			 	
+			}
+	}
+
+	public static int PrintArray(int count,int input) {
+		
+		if(input == 1){
+			return count;
+			}
+			else{
+				System.out.print(Num[count-input+1]+", ");
+				return PrintArray(count,--input);	 	
+			}
+		
+	}
+	public static void main(String[] args) {
+		int x = 0;
+		x=AddtoArray(x);
+		int input=x+1;
+		PrintArray(x,input);
+	}	
 }
