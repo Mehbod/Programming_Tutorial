6e43a1dbf6b9592b802091e026757f3f41fa552b
Mehbod <mehrbod.manavi@uni-weimar.de>
2016-12-02 18:09:08 +0100

    First part is fixed and completed

--- Strings/src/Expand.java
+++ Strings/src/Expand.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 //todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
 		//expand the String by printing each letter so many times as the number before the letter indicates
 		//e.g. 2a3b5c -> aabbbccccc
-		//Hint: first start with inputs where there is always a number and then a character, then think about how
+		//+Hint: first start with inputs where there is always a number and then a character, then think about how
 		//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
 		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
 		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee 
@@ -12,7 +12,7 @@ public class Expand {
 	public static String inputt;
 
 	public static int Expend(int lenght,int x){
-		if(x==lenght){
+		if(x>=lenght){
 			return 0;
 		}else{
 			char chr=inputt.charAt(x);
@@ -24,9 +24,9 @@ public class Expand {
 				}
 			}else{
 				System.out.print(inputt.charAt(x));
-				x++;
+				
 			}
-			
+			x++;
 			return Expend(lenght,++x);
 		}
 		
