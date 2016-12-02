250df53fb725add5b68d8e08bc8380cad4bcf22d
Mehbod <mehrbod.manavi@uni-weimar.de>
2016-12-02 17:55:01 +0100

    First part is completed

--- Strings/src/Expand.java
+++ Strings/src/Expand.java
@@ -1,19 +1,45 @@
 import java.util.Scanner;
 
 //advanced
-
-public class Expand {
-	
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
+//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
 		//expand the String by printing each letter so many times as the number before the letter indicates
 		//e.g. 2a3b5c -> aabbbccccc
 		//Hint: first start with inputs where there is always a number and then a character, then think about how
 		//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
 		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
-		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee  
+		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee 
+public class Expand {
+	public static String inputt;
+
+	public static int Expend(int lenght,int x){
+		if(x==lenght){
+			return 0;
+		}else{
+			char chr=inputt.charAt(x);
+			if(Character.isDigit(chr)){
+				int i=Character.getNumericValue(chr);
+				
+				for(int j=0;j<i;j++){
+					System.out.print(inputt.charAt(x+1));
+				}
+			}else{
+				System.out.print(inputt.charAt(x));
+				x++;
+			}
+			
+			return Expend(lenght,++x);
+		}
+		
+	}
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please Enter a name: ");
+		inputt=scan.next();
+		int lenght=inputt.length();
+		int x=0;
+		Expend(lenght,x);
+		
+		 
 		
 	}
 	
