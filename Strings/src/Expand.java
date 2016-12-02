3e780c626a316ccdc31260ef14efe927492a9766
Mehbod <mehrbod.manavi@uni-weimar.de>
2016-12-02 18:42:28 +0100

    Expand is completed in all part

--- Strings/src/Expand.java
+++ Strings/src/Expand.java
@@ -6,30 +6,49 @@ import java.util.Scanner;
 		//e.g. 2a3b5c -> aabbbccccc
 		//+Hint: first start with inputs where there is always a number and then a character, then think about how
 		//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
-		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
+		//+Further modification: if a character should only be printed one time you don't need to write any number infront of that character
 		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee 
 public class Expand {
 	public static String inputt;
 
 	public static int Expend(int lenght,int x){
+		char chr;
+		char chr2 = 0;
+		char chr3 = 0;
 		if(x>=lenght){
 			return 0;
 		}else{
-			char chr=inputt.charAt(x);
+			chr=inputt.charAt(x);
+			if(x+1<lenght){
+				chr2=inputt.charAt(x+1);
+				}
+			if(x+2<lenght){
+				chr3=inputt.charAt(x+2);
+				}
 			if(Character.isDigit(chr)){
 				int i=Character.getNumericValue(chr);
-				
+				if(Character.isDigit(chr2)){
+					i*=10;
+					int z=Character.getNumericValue(chr2);
+					i+=z;
+					x++;
+					if(Character.isDigit(chr3)){
+						i*=10;
+						z=Character.getNumericValue(chr3);
+						i+=z;
+						x++;
+					}
+				}					
 				for(int j=0;j<i;j++){
 					System.out.print(inputt.charAt(x+1));
 				}
+				x++;				
 			}else{
-				System.out.print(inputt.charAt(x));
-				
-			}
-			x++;
+				System.out.print(inputt.charAt(x));			
+			}	
+			
 			return Expend(lenght,++x);
-		}
-		
+		}		
 	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -37,10 +56,6 @@ public class Expand {
 		inputt=scan.next();
 		int lenght=inputt.length();
 		int x=0;
-		Expend(lenght,x);
-		
-		 
-		
-	}
-	
+		Expend(lenght,x);		
+	}	
 }
