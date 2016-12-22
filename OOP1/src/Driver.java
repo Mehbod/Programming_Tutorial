db5f5fc33ebdef4998b95563c03400e47ad03388
Mehbod <mehrbod.manavi@uni-weimar.de>
2016-12-22 20:08:36 +0100

    oop! not completed

--- OOP1/src/Driver.java
+++ OOP1/src/Driver.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Driver {
 	
@@ -5,8 +6,20 @@ public class Driver {
 		
 		Student a = new Student("Jakoby", 123456);
 		Professor b=new Professor("Horneker","HCI");
-		a.addLecture("Stooryboard");
-		b.addLecture("Stooryboard");
+		System.out.println(a.getName()+ " , "+a.getmatrNr());
+		System.out.println(b.getName()+" , " +b.getchair());
+		System.out.println("please Enter id number of student");
+		Scanner scan = new Scanner(System.in);
+		int inputt=scan.nextInt();
+		a.setmatrNr(inputt);
+		System.out.println("please Enter name of student");
+		String inputt2=scan.next();
+		a.setName(inputt2);
+		
+		System.out.println(a.getName()+ " , "+a.getmatrNr());
+		System.out.println(b.getName()+" , " +b.getchair());
+		//a.addLecture("Stooryboard");
+		//b.addLecture("Stooryboard");
 		
 		//todo: create at least 1 student, 1 professor and one lecture
 		// add the lecture to the student and the professor
